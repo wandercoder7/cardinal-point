467a78e1fc3ebcd2ae23f0a63bbc8ccbaeffe67a
wandercoder7 <prabhat.kataria@yahoo.com>
2025-07-30 05:30:39 +0000

    removed double logging

--- utils/logger.py
+++ utils/logger.py
@@ -14,16 +14,16 @@ def get_logger(name):
     logger.setLevel(logging.INFO)
     
     # Create console handler and set level to info
-    ch = logging.StreamHandler()
-    ch.setLevel(logging.INFO)
+    # ch = logging.StreamHandler()
+    # ch.setLevel(logging.INFO)
     
     # Create formatter
     formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
     
-    # Add formatter to ch
-    ch.setFormatter(formatter)
+    # # Add formatter to ch
+    # ch.setFormatter(formatter)
     
-    # Add ch to logger
-    logger.addHandler(ch)
+    # # Add ch to logger
+    # logger.addHandler(ch)
     
     return logger
