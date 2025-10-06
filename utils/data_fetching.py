8dc6c77c5c4c2a14950dca971e3e5cc52ce7136a
wandercoder7 <prabhat.kataria@yahoo.com>
2025-10-06 10:14:28 +0000

    Fix function name typo in data fetching and update caching decorator

--- utils/data_fetching.py
+++ utils/data_fetching.py
@@ -9,7 +9,7 @@ import traceback
 
 log = get_logger(__name__)
 
-@st.cache_data
+# @st.cache_data
 def fetch_stock_data(ticker, period="1y", interval="1d", as_of_date=None):
     """
     Fetches historical stock data from Yahoo Finance.
@@ -28,7 +28,7 @@ def fetch_stock_data(ticker, period="1y", interval="1d", as_of_date=None):
         if(as_of_date is not None):
             start = get_start_date(period, as_of_date).date()
             end = get_end_date(as_of_date, interval).date()
-            data = get_ticker_date(ticker, start, end, interval, auto_adjust_data)
+            data = get_ticker_data(ticker, start, end, interval, auto_adjust_data)
         else:
             log.info(f"fetching data for {ticker} with period {period} and interval {interval}")
             data = yf.download(ticker, period=period, interval=interval, auto_adjust=auto_adjust_data, multi_level_index=False)
@@ -74,6 +74,6 @@ def cleanup_columns(data):
     return data
 
 @st.cache_data
-def get_ticker_date(ticker, start, end, interval, auto_adjust_data):
+def get_ticker_data(ticker, start, end, interval, auto_adjust_data):
     log.info(f"fetching data for {ticker} from {start} to {end} with interval {interval}")
     return yf.download(ticker, start=start, end=end, interval=interval, auto_adjust=auto_adjust_data, multi_level_index=False)
